54b2c262424f8b0db4533a163d94112814a00936
joyance12197 <102609954+joyance12197@users.noreply.github.com>
2022-04-11 04:11:07 +0800

    Update OrderController.cs

--- TFM104MVC/TFM104MVC/Controllers/OrderController.cs
+++ TFM104MVC/TFM104MVC/Controllers/OrderController.cs
@@ -1,22 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using TFM104MVC.Dtos;
+using TFM104MVC.Services;
 
 namespace TFM104MVC.Controllers
 {
-    public class OrderController : Controller
+    [ApiController]
+    [Route("api/orders")]
+    public class OrdersController : ControllerBase
     {
-        public IActionResult Manage()
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+        public OrdersController(IHttpContextAccessor httpContextAccessor,
+            IProductRepository productRepository,
+            IMapper mapper)
         {
-            return View();
+            _httpContextAccessor = httpContextAccessor;
+            _productRepository = productRepository;
+            _mapper = mapper;
         }
 
-        public IActionResult Detail()
+
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = "Bearer")]
+        public async Task<IActionResult> GetOrders()
         {
-            return View();
+            var userId = _httpContextAccessor
+                .HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).GetHashCode();
+
+            var orders = await _productRepository.GetOrderByUserId(userId);
+            return Ok(_mapper.Map<IEnumerable<OrderDto>>(orders));
         }
 
-        public IActionResult Edit()
+        [HttpGet("{orderId}")]
+        [Authorize(AuthenticationSchemes = "Bearer")]
+        public async Task<IActionResult> GetOrderById([FromRoute] Guid orderId)
         {
-            return View();
+            var userId = _httpContextAccessor
+                .HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            var order = await _productRepository.GetOrderById(orderId);
+
+            return Ok(_mapper.Map<OrderDto>(order));
         }
     }
 }
+
